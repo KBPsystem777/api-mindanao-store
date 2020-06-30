87adba2eab9aa83985c71ebb163ed17e7bcf85cf
kbpsystem <koleen.bp@outlook.com>
2020-07-01 03:59:30 +0800

    customizing for mindanao-store

--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
+    _id: { type: String },
+    email: { type: String, trim: true, required: true, unique: true },
     username: {
       type: String,
       required: true,
@@ -11,6 +12,9 @@ const userSchema = new Schema(
       trim: true,
       minlenght: 3,
     },
+    name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    address: { type: String, required: true },
   },
   { timestamps: true }
 );
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,60 @@
 const router = require("express").Router();
+const shortid = require("shortid");
 
 let User = require("../models/userModel");
 
 router.route("/").get((req, res) => {
   User.find()
-    .then((users) => res.json(users))
-    .catch((err) => res.status(400).json(`Error: ` + err));
+    .then((users) =>
+      res.json({
+        success: true,
+        time: new Date(),
+        users: users,
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        success: false,
+        time: new Date(),
+        error_message: err,
+      })
+    );
 });
 
 router.route("/add").post((req, res) => {
+  const _id = `u-${shortid.generate()}`;
   const username = req.body.username;
+  const email = req.body.email;
+  const name = req.body.name;
+  const last_name = req.body.last_name;
+  const address = req.body.address;
 
-  const newUser = new User({ username });
+  const newUser = new User({
+    _id,
+    username,
+    email,
+    name,
+    last_name,
+    address,
+  });
 
   newUser
     .save()
-    .then(() => res.json("User Added!"))
-    .catch((err) => res.status(400).json(`Error: ` + err));
+    .then(() =>
+      res.json({
+        success: true,
+        message: "User Added!",
+        newUser,
+        time: new Date(),
+      })
+    )
+    .catch((err) =>
+      res.status(400).json({
+        success: false,
+        message: `Error: ` + err,
+        time: new Date(),
+      })
+    );
 });
 
 module.exports = router;

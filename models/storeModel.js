d7c4c630f6f561b5a1bd990beede45a3abe0d06c
kbpsystem <koleen.bp@outlook.com>
2020-07-06 00:05:59 +0800

    build routes and support for Stores

---
+++ models/storeModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const storeSchema = new Schema(
+  {
+    _id: { type: String },
+    active: { type: Boolean },
+    store_name: { type: String, required: true, minlength: 3, unique: true },
+    store_email: { type: String, minlength: 4, unique: true },
+    mobile: { type: String, minlength: 8, required: true },
+    store_address: { type: String, required: true },
+    store_owner: { type: String, required: true },
+    store_owner_email: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Store = mongoose.model("Store", storeSchema);
+
+module.exports = Store;

e473989ecf2f6f0dccb0ea160d6e61181069943a
kbpsystem <koleen.bp@outlook.com>
2020-07-06 17:12:40 +0800

    adding store_details on the storeModel

--- models/storeModel.js
+++ models/storeModel.js
@@ -5,11 +5,18 @@ const storeSchema = new Schema(
   {
     _id: { type: String },
     active: { type: Boolean },
-    store_name: { type: String, required: true, minlength: 3, unique: true },
+    store_name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      unique: true,
+      trim: true,
+    },
     store_email: { type: String, minlength: 4, unique: true },
     mobile: { type: String, minlength: 8, required: true },
     store_address: { type: String, required: true },
-    store_owner: { type: String, required: true },
+    store_owner_username: { type: String },
+    store_owner_id: { type: String },
     store_owner_email: { type: String, required: true },
   },
   { timestamps: true }
